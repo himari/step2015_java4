4717da05f0c7383c5b390da69ab7d53bf87e92b3
himari <sunflowerhimari@gmail.com>
2015-06-24 20:18:11 +0900

    Update MatrixTest.java

--- MatrixTest.java
+++ MatrixTest.java
@@ -10,36 +10,21 @@ public class MatrixTest {
 	public void test() {
 
 	double [][] a={{1,0,0},{0,1,0},{0,0,1}};
-
 	double [][] b={{1,0,0},{0,2,0},{0,0,3}};
-
 	double [][] c={{1,0,0},{0,2,0},{0,0,3}};
-
 	double [][] d={{1.00,0.1,10},{0,0,0},{0,0,0}};
-
-	double [][] e={{2147483647,0,0},{0,-2,0},{0,0,-1}};
-
-	double [][] f={{2147483647,-0.2,-10},{0,0,0},{0,0,0}};
-
+	double [][] e={{500000,0,0},{0,-2,0},{0,0,-1}};
+	double [][] f={{500000,-0.2,-10},{0,0,0},{0,0,0}};
 	double [][] g={{1}};
-
 	double [][] h={{2}};
-
 	double [][] k={{2}};
 
-	//readmat check
-
-	//assertNotNull();
-
-	//cal check
+ 	//checks matrixreader
 
 	assertArrayEquals(c,(Matrix.calc(a,b,3)));
-
 	assertArrayEquals(f,(Matrix.calc(d,e,3)));
-
 	assertArrayEquals(k,(Matrix.calc(g,h,1)));
-
-	//main
+	//calculation
 
 	//assertEquals();
 
