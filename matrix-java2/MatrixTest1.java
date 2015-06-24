f454d43f0470ad1ddab96658ae474818795bc2a4
himari <sunflowerhimari@gmail.com>
2015-06-24 21:12:56 +0900

    Update MatrixTest1.java

--- matrix-java2/MatrixTest1.java
+++ matrix-java2/MatrixTest1.java
@@ -7,7 +7,7 @@ public class MatrixTest2 {
 	
 	@Test
 	public void test() {
-		    assertEquals(450.0, Matrix.main(3), 0);
+		assertEquals(450.0, Matrix.main(3), 0);
 	        assertEquals(3680.0, Matrix.main(4), 0);
 	        assertEquals(18250.0, Matrix.main(5), 0);
 	        assertEquals(1953111979187144450.000000, Matrix.main(500), 0286000);
